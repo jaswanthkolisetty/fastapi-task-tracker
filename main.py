ab480203c4be0e34a565d61f2f4fff9c2cb5f64c
Jaswanth Kolisetty <jaswanthkolisetty@gmail.com>
2026-04-22 00:58:18 -0500

    Initial FastAPI task tracker setup

---
+++ main.py
@@ -0,0 +1,18 @@
+from fastapi import FastAPI
+
+app = FastAPI()
+
+
+@app.get("/")
+def read_root():
+    return {"message": "FastAPI Task Tracker is running"}
+
+
+@app.get("/tasks")
+def get_tasks():
+    return {
+        "tasks": [
+            {"id": 1, "title": "Learn Git basics", "done": False},
+            {"id": 2, "title": "Push first commit", "done": False},
+        ]
+    }
